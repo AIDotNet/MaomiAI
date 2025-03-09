c1ba76923ea35e5bef83d5d903921134de17cb52
whuanle <1586052146@qq.com>
2025-03-09 22:08:04 +0800

    添加部分表

---
+++ src/database/MaomiAI.Database.Postgres/Configurations/TeamAiModelConfiguration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using MaomiAI.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MaomiAI.Database;
+
+/// <summary>
+/// ai模型.
+/// </summary>
+public partial class TeamAiModelConfiguration : IEntityTypeConfiguration<TeamAiModelEntity>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<TeamAiModelEntity> builder)
+    {
+        var entity = builder;
+        entity.HasKey(e => e.Id).HasName("team_ai_model_pk");
+
+        entity.ToTable("team_ai_model", tb => tb.HasComment("ai模型"));
+
+        entity.Property(e => e.Id)
+            .HasDefaultValueSql("uuid_generate_v4()")
+            .HasComment("id")
+            .HasColumnName("id");
+        entity.Property(e => e.CreateTime)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP")
+            .HasComment("创建时间")
+            .HasColumnName("create_time");
+        entity.Property(e => e.CreateUserId)
+            .HasComment("创建人ID")
+            .HasColumnName("create_user_id");
+        entity.Property(e => e.DeploymentName)
+            .HasMaxLength(50)
+            .HasComment("部署名称")
+            .HasColumnName("deployment_name");
+        entity.Property(e => e.Endpoint)
+            .HasMaxLength(100)
+            .HasComment("api服务端点")
+            .HasColumnName("endpoint");
+        entity.Property(e => e.IsDeleted)
+            .HasDefaultValue(false)
+            .HasComment("是否删除")
+            .HasColumnName("is_deleted");
+        entity.Property(e => e.IsSupportImg)
+            .HasDefaultValue(false)
+            .HasComment("是否支持图片")
+            .HasColumnName("is_support_img");
+        entity.Property(e => e.Key)
+            .HasMaxLength(100)
+            .HasComment("key")
+            .HasColumnName("key");
+        entity.Property(e => e.ModeId)
+            .HasMaxLength(50)
+            .HasComment("模型id")
+            .HasColumnName("mode_id");
+        entity.Property(e => e.ModelType)
+            .HasComment("模型类型")
+            .HasColumnName("model_type");
+        entity.Property(e => e.Provider)
+            .HasMaxLength(20)
+            .HasComment("ai供应商")
+            .HasColumnName("provider");
+        entity.Property(e => e.TeamId)
+            .HasComment("团队id")
+            .HasColumnName("team_id");
+        entity.Property(e => e.UpdateTime)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP")
+            .HasComment("更新时间")
+            .HasColumnName("update_time");
+        entity.Property(e => e.UpdateUserId)
+            .HasComment("更新人ID")
+            .HasColumnName("update_user_id");
+
+        OnConfigurePartial(entity);
+    }
+
+    partial void OnConfigurePartial(EntityTypeBuilder<TeamAiModelEntity> modelBuilder);
+}
---
+++ src/database/MaomiAI.Database.Shared/Entities/TeamWiki.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using MaomiAI.Database.Audits;
+
+namespace MaomiAI.Database.Entities;
+
+/// <summary>
+/// 知识库.
+/// </summary>
+public partial class TeamWikiEntity : IFullAudited
+{
+    /// <summary>
+    /// id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// 团队id.
+    /// </summary>
+    public Guid TeamId { get; set; }
+
+    /// <summary>
+    /// 知识库名称.
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// 知识库描述.
+    /// </summary>
+    public string Description { get; set; } = null!;
+
+    /// <summary>
+    /// 绑定的向量模型id.
+    /// </summary>
+    public Guid ModelId { get; set; }
+
+    /// <summary>
+    /// 是否删除.
+    /// </summary>
+    public bool IsDeleted { get; set; }
+
+    /// <summary>
+    /// 创建时间.
+    /// </summary>
+    public DateTimeOffset CreateTime { get; set; }
+
+    /// <summary>
+    /// 更新时间.
+    /// </summary>
+    public DateTimeOffset UpdateTime { get; set; }
+
+    /// <summary>
+    /// 创建人ID.
+    /// </summary>
+    public Guid CreateUserId { get; set; }
+
+    /// <summary>
+    /// 更新人ID.
+    /// </summary>
+    public Guid UpdateUserId { get; set; }
+}
