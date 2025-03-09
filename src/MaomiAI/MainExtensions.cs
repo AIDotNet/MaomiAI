be2e65f9c714cac1b9544ae34d78e34cf6ed60bd
whuanle <1586052146@qq.com>
2025-03-09 23:04:02 +0800

    增加日志

--- src/MaomiAI/MainExtensions.cs
+++ src/MaomiAI/MainExtensions.cs
@@ -1,7 +1,11 @@
 ﻿using Maomi;
-using Microsoft.Extensions.Configuration;
 using Serilog;
+
+#if !DEBUG
+using Microsoft.Extensions.Configuration;
 using Serilog.Extensions.Logging;
+using Serilog.Formatting.Compact;
+#endif
 
 namespace MaomiAI;
 
@@ -20,11 +24,11 @@ public static class MainExtensions
             configuration.ReadFrom.Configuration(builder.Configuration);
 #else
             // 使用 json 收集时，忽略日志模板
-            ctx.Configuration["Serilog:WriteTo:0:Args:outputTemplate"] = "";
+            //configuration["Serilog:WriteTo:0:Args:outputTemplate"] = "";
 
-            // 以 json 格式化输出
-            configuration.WriteTo.Console(new RenderedCompactJsonFormatter())
-            .ReadFrom.Configuration(ctx.Configuration);
+            //// 以 json 格式化输出
+            //configuration.WriteTo.Console(new RenderedCompactJsonFormatter())
+            //.ReadFrom.Configuration(configuration);
 #endif
         });
 
